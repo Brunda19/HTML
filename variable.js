2c734def621abfa8c8d2c6dc562a3d063205b95e
Brunda19 <brundapn19@gmail.com>
2024-12-20 12:16:18 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,52 @@
+/*let a = 10;
+a=20;
+console.log("a",a);*/
+
+/*const b="hello"
+b="hello"
+console.log("b",b);*/
+
+/*var c = 1.25;
+c = 2.5;
+console.log("c",c);*/
+
+/*const data = null;
+console.log("data",typeof data);*/
+
+/*const data = undefined;
+console.log("data",typeof data);*/
+
+/*const data = 12;
+console.log("data type", data,"is", typeof data);*/
+
+/*let data = "arun";
+console.log("data type of",data,"is", typeof data);*/
+
+/*data = Symbol("cat");
+console.log("data type of",data,"is",typeof data);*/
+
+/*data = Array(1,2,3,true,'mango',undefined,null);
+data =[2,4,false,'orange',undefined,null];
+console.log("data",typeof data);*/
+
+/*data = {
+    name:"Anu",
+    address:"Shivamogga",
+    age:23,
+    phone:true,
+    education:null
+}
+console.log("name",data.name);
+console.log("data type of",data,"is",typeof data);
+
+let user = prompt("Enter your string");
+console.log("user",user);*/
+
+let a = 10;
+let b = 20;
+console.log(a*b);
+
+/*var c =2.3;
+d =5.6;
+console.log(c+d);*/
+
